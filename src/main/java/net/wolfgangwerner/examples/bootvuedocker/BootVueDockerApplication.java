7a2f39d68175af0d2fd7501fd647d05b01426f40
wwerner <mail@wolfgang-werner.net>
2019-02-02 11:32:02 +0100

    Add newly scaffolded spring boot project

---
+++ src/main/java/net/wolfgangwerner/examples/bootvuedocker/BootVueDockerApplication.java
@@ -0,0 +1,14 @@
+package net.wolfgangwerner.examples.bootvuedocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootVueDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootVueDockerApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/net/wolfgangwerner/examples/bootvuedocker/BootVueDockerApplicationTests.java
@@ -0,0 +1,17 @@
+package net.wolfgangwerner.examples.bootvuedocker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootVueDockerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
